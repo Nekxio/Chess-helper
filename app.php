d6e0fa4aa6bdbd9af8d328004295356f725f0d9a
Nekxio <63882847+Nekxio@users.noreply.github.com>
2022-10-27 18:31:48 +0200

    Fix resultats en dehors de l'échiquier

--- app.php
+++ app.php
@@ -15,7 +15,7 @@
 // chaque classe a des getter/setter
 //Créer une fonction qui permet d'instancier une pièce en précisant son type et sa position sur l'echiquier
 //Créer une fonction qui permet d'afficher tous les déplacements possibles de la pièce en fonction de l'emplacement donnée par l'utilisateur
-
+//Chaque résultat est contenu entre A1 et H8 
 class Piece {
     protected $position;
     public function __construct($position) {
@@ -68,11 +68,11 @@ class Fou extends Piece {
     public function deplacementPossible() {
         $position = $this->getPosition();
         $deplacementPossible = array();
-        for ($i = 1; $i < 8; $i++) {
-                $deplacementPossible[] = chr(ord($position[0]) + $i) . ($position[1] + $i);
-                $deplacementPossible[] = chr(ord($position[0]) + $i) . ($position[1] - $i);
-                $deplacementPossible[] = chr(ord($position[0]) - $i) . ($position[1] - $i);
-                $deplacementPossible[] = chr(ord($position[0]) - $i) . ($position[1] + $i);
+        for ($i = 1; $i <= 8; $i++) {
+            $deplacementPossible[] = chr(ord($position[0]) + $i) . ($position[1] + $i);
+            $deplacementPossible[] = chr(ord($position[0]) + $i) . ($position[1] - $i);
+            $deplacementPossible[] = chr(ord($position[0]) - $i) . ($position[1] + $i);
+            $deplacementPossible[] = chr(ord($position[0]) - $i) . ($position[1] - $i);
         }
         return $deplacementPossible;
     }
@@ -183,13 +183,15 @@ $echiquier->afficherEchiquier();
 
 
 //2. On demande à l'utilisateur de choisir une pièce et une position
-
-$choixPiece = readline('Choisissez une pièce : ');
-$position = readline('Choisissez une position : ');
+echo "Bienvenue dans le simulateur de déplacement aux jeux d'echec" . PHP_EOL;
+echo"Les pièces sont : " . PHP_EOL;
+echo "pion, cavalier, fou, tour, reine & roi" . PHP_EOL;
+$choixPiece = readline('Choisissez la pièce de votre choix : ');
+$position = readline("Choisissez sa position sur l'echiquier (ex: d4): ");
 //On vérifie que le choix de la pièce correspond à l'une de l'echiquier
     if ($choixPiece == 'pion' || $choixPiece == 'cavalier' || $choixPiece == 'fou' || $choixPiece == 'tour' || $choixPiece == 'reine' || $choixPiece == 'roi') {
         //On vérifie que la position est bien sur l'echiquier
-        if (ord($position[0]) >= 97 && ord($position[0]) <= 104 && $position[1] >= 1 && $position[1] <= 8) {
+        if (preg_match('/^[a-h][1-8]$/', $position)) {
             //On instancie la pièce
             $piece = new $choixPiece($position);
             //On récupère les déplacements possibles
@@ -198,7 +200,15 @@ $position = readline('Choisissez une position : ');
             $echiquier->afficherEchiquier();
             echo "Il est possible de jouer cette pièce en :".PHP_EOL ;
             foreach ($deplacementPossible as $deplacement) {
+            // On vérifie que le déplacement est bien sur l'echiquier
+            if (preg_match('/^[a-h][1-8]$/', $deplacement)){
                 echo $deplacement . PHP_EOL;
+            }
+            // Sinon on affiche un message pour informer de l'impossibilité du déplacement ou pour informer que tous les déplacements ont été affichés
+            else {
+                echo "Il n'est pas possible de déplacer la pièce plus loin sur l'echiquier" . PHP_EOL;
+                break;
+            }
             }
         } else {
             echo 'La position n\'est pas sur l\'echiquier';
@@ -206,3 +216,4 @@ $position = readline('Choisissez une position : ');
     } else {
         echo 'La pièce n\'existe pas';
     }
+?>
